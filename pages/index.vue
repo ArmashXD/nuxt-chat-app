8c65be35f870dcbaed1a4fd35ca330e35c38f43f
Armash <armashash@gmail.com>
2021-12-04 17:05:00 +0500

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+   <div class="container  mt-5">
+     <h4>Chat App</h4>
+     <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" >
+    <div  class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+      <input  class="fs-5 fw-semibold form-control" v-model="username">
+    </div>
+    <div class="list-group list-group-flush border-bottom scrollarea">
+      <div  class="list-group-item list-group-item-action py-3 lh-tight" 
+      v-for="message in messages" :key="message"
+      >
+        <div class="d-flex w-100 align-items-center justify-content-between">
+          <strong class="mb-1">{{message.username}}</strong>
+        </div>
+        <div class="col-10 mb-1 small">{{message.message}}</div>
+      </div>
+    </div>
+  </div>
+  <form @submit.prevent="submit">
+    <input class="form-control" placeholder="Write a message" v-model="message"/>
+  </form>
+   </div>
+</template>
+
+<script>
+import Pusher from 'pusher-js';
+
+export default {
+  data(){
+    return{
+      username:'username',
+      message:'',
+      messages:[]
+    }
+  },
+  mounted(){
+  // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('e729c0541318a65b17d9', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', data => {
+      this.messages.push(data);
+    });
+  },
+  methods:{
+    async submit(){
+      //connect your backend api here 
+      await this.$axios.post('http://localhost:8000/api/messages',{
+        username:this.username,
+        message:this.message
+      });
+
+      this.message = '';
+    },
+  }
+}
+</script>
+
+<style>
+  .scrollarea{
+    min-height: 500px;
+  }
+</style>
